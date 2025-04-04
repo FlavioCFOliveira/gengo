94270ac6fa1353e1cb3c4ace99a69d0ed23ac227
flavio cf oliveira <geral@flaviooliveira.com>
2025-04-04 21:37:49 +0100

    refactor date functions to use randomUnixBetween for improved readability

--- dates.go
+++ dates.go
@@ -5,27 +5,44 @@ import (
 	"time"
 )
 
+//	func Date() time.Time {
+//		randomUnix := rand.Int63n(dtMax-dtMin+1) + dtMin
+//		return time.Unix(randomUnix, 0).UTC()
+//	}
+//
+//	func DateBetween(start, end time.Time) time.Time {
+//		if start.After(end) {
+//			start, end = end, start
+//		}
+//
+//		startUnix := start.Unix()
+//		endUnix := end.Unix()
+//
+//		// Garante que o range não está vazio
+//		if startUnix == endUnix {
+//			return start
+//		}
+//
+//		// Gera um número aleatório entre startUnix e endUnix
+//		randomUnix := rand.Int63n(endUnix-startUnix+1) + startUnix
+//
+//		// Converte para time.Time
+//		return time.Unix(randomUnix, 0).UTC()
+//	}
+func randomUnixBetween(startUnix, endUnix int64) int64 {
+	if startUnix == endUnix {
+		return startUnix
+	}
+	return rand.Int63n(endUnix-startUnix+1) + startUnix
+}
+
 func Date() time.Time {
-	randomUnix := rand.Int63n(dtMax-dtMin+1) + dtMin
-	return time.Unix(randomUnix, 0).UTC()
+	return time.Unix(randomUnixBetween(dtMin, dtMax), 0).UTC()
 }
 
 func DateBetween(start, end time.Time) time.Time {
 	if start.After(end) {
 		start, end = end, start
 	}
-
-	startUnix := start.Unix()
-	endUnix := end.Unix()
-
-	// Garante que o range não está vazio
-	if startUnix == endUnix {
-		return start
-	}
-
-	// Gera um número aleatório entre startUnix e endUnix
-	randomUnix := rand.Int63n(endUnix-startUnix+1) + startUnix
-
-	// Converte para time.Time
-	return time.Unix(randomUnix, 0).UTC()
+	return time.Unix(randomUnixBetween(start.Unix(), end.Unix()), 0).UTC()
 }
