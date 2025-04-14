7048726a9edb070f977220a6db9a7dc4bcca4531
flavio cf oliveira <geral@flaviooliveira.com>
2025-04-14 10:06:38 +0100

    the package "math/rand/v2" has a automatic seed initialization

--- init.go
+++ init.go
@@ -2,12 +2,10 @@ package gengo
 
 import (
 	"errors"
-	"math/rand"
 	"time"
 )
 
 var (
-	rnd                *rand.Rand
 	ErrorNilFunc       = errors.New("f cannot be nil")
 	ErrorFuncNilResult = errors.New("f returned nil")
 )
@@ -15,27 +13,8 @@ var (
 var dtMin, dtMax, dtUnixMin, dtUnixMax int64
 
 func init() {
-	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	dtMin = time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC).Unix()
 	dtMax = time.Date(9999, 12, 31, 23, 59, 59, 0, time.UTC).Unix()
 	dtUnixMin = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC).Unix()
 	dtUnixMax = time.Date(2038, 01, 19, 3, 14, 07, 0, time.UTC).Unix()
-}
-
-func Reseed() {
-	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
-}
-
-func ReseedFunc(f func() *rand.Rand) error {
-	if f == nil {
-		return ErrorNilFunc
-	}
-
-	newRnd := f()
-	if newRnd == nil {
-		return ErrorFuncNilResult
-	}
-	rnd = newRnd
-
-	return nil
 }
